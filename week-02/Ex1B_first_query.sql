161750b5b8452d727a1838eebc649f1534f3e382
destiney lomeli <dlomeli@my.yearupunited.org>
2026-04-14 21:56:28 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Salkia is a database used to collect information about business style data and it's used to analyze how that business operates.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
