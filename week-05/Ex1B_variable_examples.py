019d65595459e68933fc6434fcde2bfa43022c28
Destiney L <destineylomeli122@gmail.com>
2026-05-05 12:55:29 -0700

    Completed Ex1B variable examples and Lab 2

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,25 @@
+# Customer information 
+customer_id = 101
+customer_name = "Ashley E" # I combined first and last name, but they could be separate variables
+customer_gender = "Female" # Could also be stores as M/F or another format
+customer_date_of_birth = "01/01/2000" # stored as a string, but could use a date format
+drivers_license_number = "D1234567" # Format may cary by state
+auto_policy_number = "AP987654" # Example value, real ones may follow a specific pattern
+
+# My information
+my_name = "Destiney"
+city_state = "Watsonville, CA"
+
+# Lab 2
+
+ # a) Reserved words for Python:
+    # False, None, True, and, as, assert, async, await, break, class, continue,
+    # def, del, elif, else, except, finally, for, from, global, if, import,
+    # in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield 
+
+# b) Pick 5 and explain:
+    # def: used to define a function
+    # if: sued to make decisions (conditional statements)
+    # for: used to create loops
+    # return: used to send a value back from a function
+    # import: used to bring in external libraries or modules
