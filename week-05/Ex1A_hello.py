197de5fd8c4ea89a2bd16e572434b51c5198e478
Destiney L <destineylomeli122@gmail.com>
2026-05-05 12:30:35 -0700

    Completed first Python file

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,23 @@
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world!" prints twice because we used the print() function which is used to display a value or result.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = 0.50
+
+print(dollars + cents)
+
+# The result is 3.5 instead of 3.50 because Python does not automatically format decimals to show two places.
+
+cents = cents + 0.25
+print(dollars + cents)
+
+d_str = "3 dollars"
+c_str = "50 cents"
+
+print(d_str + " " + c_str)
